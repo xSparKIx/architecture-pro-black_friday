7d59418cfbd6b73208e2b89570a91ba552885414
SparKI <sparki.projects@yandex.ru>
2025-10-01 00:28:37 +0300

    2 задание и 3

--- tasks/task3/mongo-sharding-repl/scripts/mongo-init.sh
+++ tasks/task3/mongo-sharding-repl/scripts/mongo-init.sh
@@ -26,12 +26,14 @@ until docker compose exec -T config1 mongosh --quiet --port 27017 --eval "db.hel
 done
 
 # Инициализируем реплики шарда 1
-docker compose exec -T shard1 mongosh --port 27018 --quiet <<EOF
+docker compose exec -T shard1_a mongosh --port 27018 --quiet <<EOF
 rs.initiate(
     {
       _id : "shard1",
       members: [
-        { _id : 0, host : "shard1:27018" }
+        { _id : 0, host : "shard1_a:27018" },
+        { _id : 1, host : "shard1_b:27018" },
+        { _id : 2, host : "shard1_c:27018" }
       ]
     }
 );
@@ -39,12 +41,14 @@ exit()
 EOF
 
 # Инициализируем реплики шарда 2
-docker compose exec -T shard2 mongosh --port 27018 --quiet <<EOF
+docker compose exec -T shard2_a mongosh --port 27018 --quiet <<EOF
 rs.initiate(
     {
       _id : "shard2",
       members: [
-        { _id : 0, host : "shard2:27018" }
+        { _id : 0, host : "shard2_a:27018" },
+        { _id : 1, host : "shard2_b:27018" },
+        { _id : 2, host : "shard2_c:27018" }
       ]
     }
 );
@@ -52,12 +56,14 @@ exit()
 EOF
 
 # Инициализируем реплики шарда 3
-docker compose exec -T shard3 mongosh --port 27018 --quiet <<EOF
+docker compose exec -T shard3_a mongosh --port 27018 --quiet <<EOF
 rs.initiate(
     {
       _id : "shard3",
       members: [
-        { _id : 0, host : "shard3:27018" }
+        { _id : 0, host : "shard3_a:27018" },
+        { _id : 1, host : "shard3_b:27018" },
+        { _id : 2, host : "shard3_c:27018" }
       ]
     }
 );
@@ -66,9 +72,9 @@ EOF
 
 # Инцициализируйте роутер и наполняем его тестовыми данными
 docker compose exec -T mongos_router mongosh --port 27020 --quiet <<EOF
-sh.addShard("shard1/shard1:27018")
-sh.addShard("shard2/shard2:27018)
-sh.addShard("shard3/shard3:27018)
+sh.addShard("shard1/shard1_a:27018,shard1_b:27018,shard1_c:27018")
+sh.addShard("shard2/shard2_a:27018,shard2_b:27018,shard2_c:27018")
+sh.addShard("shard3/shard3_a:27018,shard3_b:27018,shard3_c:27018")
 
 sh.enableSharding("somedb");
 sh.shardCollection("somedb.helloDoc", { "name" : "hashed" } )
